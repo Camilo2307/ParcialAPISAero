6e9a02f71f4d837f2fbe0c0a1dcc549416a39399
Christian Camilo Peña <cc.pena2@uniandes.edu.co>
2025-05-18 17:24:40 -0500

    Se envian cambios en los modulos

--- src/aeropuerto/aeropuerto.module.ts
+++ src/aeropuerto/aeropuerto.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AeropuertoService } from './aeropuerto.service';
 import { AeropuertoController } from './aeropuerto.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Aeropuerto } from './aeropuerto.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Aeropuerto])],
+  controllers: [AeropuertoController],
   providers: [AeropuertoService],
-  controllers: [AeropuertoController]
 })
 export class AeropuertoModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AerolineaModule } from './aerolinea/aerolinea.module';
 import { AeropuertoModule } from './aeropuerto/aeropuerto.module';
 
 @Module({
-  imports: [AerolineaModule, AeropuertoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'cobertura',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AerolineaModule,
+    AeropuertoModule,
+  ],
 })
 export class AppModule {}
